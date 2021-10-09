3936bdec3faa401048f62744739792950110810c
Jonathan Guzmán <jonguz5@hotmail.com>
2021-10-09 13:18:09 -0600

    mostrar ejercicios anteriores

--- src/main/java/cr/ac/una/App.java
+++ src/main/java/cr/ac/una/App.java
@@ -6,10 +6,10 @@ import cr.ac.una.ejercicios.Ejercicio3;
 
 public class App {
     public static void main(String[] args) {
-        //System.out.println("Ejercicio 1:");
-        //new Ejercicio1().run();
-        //System.out.println("Ejercicio 2:");
-        //new Ejercicio2().run();
+        System.out.println("Ejercicio 1:");
+        new Ejercicio1().run();
+        System.out.println("Ejercicio 2:");
+        new Ejercicio2().run();
         System.out.println("Ejercicio 3:");
         new Ejercicio3().run();
     }
